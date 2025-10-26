3564937e8ff5764d1500278159ac67e2dbb329c9
Luísa de Souza Lopes <51141827+luisalopes@users.noreply.github.com>
2025-10-26 14:11:16 +0000

    Primeiras variáveis e mensagens do programa.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,17 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado_a, estado_b; //Declaração das variáveis de cada estado, da carta A e carta B.
+  char codigo_a[5]; //Declarei os códigos de cada carta em linhas separadas porque não sei se a restrição de caracteres vai dar bug se ficar junta.
+  char codigo_b[5]; //Declaração de código da carta B; a de cima foi da carta A.
 
   // Área para entrada de dados
+  print("Boas-vindas ao Desafio Super Trunfo - Países!\n");
+  
+  printf("Para poder jogar o Super Trunfo, vamos primeiro precisar registrar nossas cartas no montante.\n");
 
   // Área para exibição dos dados da cidade
+  printf("Agora vamos ver as cartas que temos à nossa disposição!\n");
 
 return 0;
 } 
